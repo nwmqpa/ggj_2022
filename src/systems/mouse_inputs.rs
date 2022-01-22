7920021b81773f4227b8e0e546b64118889bf93a
Thomas Nicollet <thomas.nicollet@epitech.eu>
2022-01-22 23:20:33 +0100

    :sparkles: kargh

--- src/systems/mouse_inputs.rs
+++ src/systems/mouse_inputs.rs
@@ -1,17 +1,25 @@
 use bevy::{
-    input::mouse::{MouseButtonInput},
-    prelude::{Commands, EventReader, Res, Transform}, window::Windows, math::{Vec2, Vec3}, sprite::SpriteSheetBundle,
+    input::mouse::MouseButtonInput,
+    math::{Vec2, Vec3},
+    prelude::{Commands, EventReader, Res, Transform},
+    sprite::SpriteSheetBundle,
+    window::Windows,
 };
 
-use crate::{components::Position, animations::AnimationHandles};
+use crate::{animations::AnimationHandles, components::Position};
 
-fn spawn_monster(commands: &mut Commands, position: Vec2, animation_handles: &Res<AnimationHandles>) {
+fn spawn_monster(
+    commands: &mut Commands,
+    position: Vec2,
+    animation_handles: &Res<AnimationHandles>,
+) {
     commands
         .spawn()
         .insert(Position::new(position.x, position.y))
         .insert_bundle(SpriteSheetBundle {
             texture_atlas: animation_handles.mummy_idle.clone_weak(),
-            transform: Transform::from_scale(Vec3::splat(0.2)),
+            transform: Transform::from_scale(Vec3::splat(0.1))
+                .with_translation(Vec3::new(position.x, position.y, 1.0)),
             ..Default::default()
         });
 }
@@ -20,7 +28,7 @@ pub fn mouse_button_events(
     mut commands: Commands,
     mut mousebtn_evr: EventReader<MouseButtonInput>,
     windows: Res<Windows>,
-    animation_handles: Res<AnimationHandles>
+    animation_handles: Option<Res<AnimationHandles>>,
 ) {
     use bevy::input::ElementState;
     let window = windows.get_primary().unwrap();
@@ -29,7 +37,9 @@ pub fn mouse_button_events(
         match ev.state {
             ElementState::Pressed => {
                 if let Some(position) = window.cursor_position() {
-                    spawn_monster(&mut commands, position, &animation_handles);
+                    if let Some(animation_handles) = animation_handles.as_ref() {
+                        spawn_monster(&mut commands, position, &animation_handles);
+                    }
                 }
             }
             _ => {}
